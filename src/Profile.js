3dfb1ea559828d2357347b14fab16190a151e377
Mikhail Semenov <msff1@me.com>
2018-06-29 10:19:46 +0300

    chore: remove unneccessary imports

--- src/Profile.js
+++ src/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 import {
-  NavLink, Route, Switch, withRouter,
+  Route, Switch, withRouter,
 } from 'react-router-dom';
 
 // Owned components import
